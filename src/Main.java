ddc557b64e34df0a4c4cf8f5b25cf892765ad376
simon <simongizaw@gmail.com>
2022-10-12 19:30:01 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.print("Enter your party affiliation ");
+        input = scanner.nextLine();
+
+        switch (input)
+        {
+            case "d":
+            case "D":
+                System.out.println("You get a Democratic Donkey ");
+            break;
+
+            case "r":
+            case "R":
+                System.out.println("You get a Republican Elephant");
+            break;
+
+            case "i":
+            case"I":
+                System.out.println("You get an Independent Person");
+            break;
+
+            default:
+                System.out.println("You get other.");
+
+            break;
+
+
+        }
+
+    }
+}
